081ef587515bc376960ae239c00b8739450c07ac
Mustafa ÖNCEL <support@lifemcserver.com>
2021-02-18 08:01:46 +0300

    Add JavaDoc to module info

--- src/main/java9/module-info.java
+++ src/main/java9/module-info.java
@@ -1,3 +1,14 @@
+/**
+ * A Java Bytecode Version Analyzer CLI program.
+ * <p>
+ * Can show the Java class file version target of the given class,
+ * classes from a JAR or ZIP archive.
+ * <p>
+ * It can display a metric like % classes use Java 8 or such,
+ * or warn legacy/bleeding edge ones, too.
+ *
+ * @moduleGraph
+ */
 module com.lifemcserver.bytecodeversionanalyzer {
     requires maven.model;
     requires plexus.utils;
